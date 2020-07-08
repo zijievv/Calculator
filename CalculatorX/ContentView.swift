cf96d3c12c3b0a4b29562ef84e01f0ca54774afa
zijievv <zijie_vv@icloud.com>
2020-07-08 23:12:37 +0800

    CAlculatorX - init

---
+++ CalculatorX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculatorX
+//
+//  Created by zijie vv on 2020-07-08.
+//  Copyright © 2020 zijie vv. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
